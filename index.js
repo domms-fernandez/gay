168cc20c10f948c4104c1d3bc31a66361aedc5b9
domms-fernandez <119355139+domms-fernandez@users.noreply.github.com>
2025-09-16 22:55:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ button.addEventListener("click", () => {
 button.addEventListener("mousemove", (e) => {
   let buttonPos = button.getBoundingClientRect(); //x and y of button
 
-  let pointA = {x: Math.round(button.clientWidth * 0.5) + 1,  y: 0}; //the x value if it intercepts the side lines,
-  let pointB = {y: Math.round(button.clientHeight * 0.5) + 1, x: 0}; //and the y value for the top and bottom lines.
+  let pointA = {x: Math.round(button.clientWidth * 0.5),  y: 0}; //the x value if it intercepts the side lines,
+  let pointB = {y: Math.round(button.clientHeight * 0.5), x: 0}; //and the y value for the top and bottom lines.
                                                                  //incidently 1/2 width/height of the button
   
   //mouse X and Y relative to the center point of the button, which is (0, 0)
